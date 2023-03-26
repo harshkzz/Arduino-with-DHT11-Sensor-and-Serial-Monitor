575d9986b8a33c673a0af91b64680a0c1a47882d
Harsh Kumar <125388784+harshkzz@users.noreply.github.com>
2023-03-26 20:38:13 +0530

    Add files via upload

---
+++ DHT11_SERIALMONITOR/DHT11_SERIALMONITOR.ino
@@ -0,0 +1,29 @@
+#include "DHT.h"
+
+#define DHTPIN 3
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop() {
+    delay(2000);  // 2 seconds delay
+  float hum = dht.readHumidity();
+  float temp = dht.readTemperature(false); //Set to true for Fahrenheit
+
+  if (isnan(hum) || isnan(temp)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+  
+  Serial.print("Temperature: ");
+  Serial.print(temp);
+  Serial.println(" C");
+  Serial.print("Humidity: ");
+  Serial.print(hum);
+  Serial.println("%");
+}
